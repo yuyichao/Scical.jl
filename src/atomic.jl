c3b0a5a8ca7a44c43331a75deb1fcbe7b46e5b42
Yichao Yu <yyc1992@gmail.com>
2014-12-21 22:37:35 -0500

    [atomic] transition

--- src/atomic.jl
+++ src/atomic.jl
@@ -17,8 +17,9 @@
 @_include_sub _const
 @_include_sub _ffi
 
+using Class
+
 export compose_g, compose_gJ, compose_gF
-export sideband_strength, sideband_scatter_strength
 
 function compose_g(J_sum::Real, J1::Real, J2::Real, g1::Real, g2::Real)
     if J_sum == 0
@@ -39,10 +40,62 @@ function compose_gF(F::Real, I::Real, J::Real, L::Real, S::Real, g_I::Real=0.0)
     return compose_g(F, I, J, g_I, compose_gJ(J, L, S))
 end
 
+export sideband_strength, sideband_scatter_strength
+
 function sideband_strength(n1, n2, eta)
     return _ffi_harmonic_recoil(n1, n2, eta)
 end
 
 function sideband_scatter_strength(n1, n2, eta, theta0)
     return _ffi_harmonic_scatter(n1, n2, eta, theta0)
+end
+
+export Transition
+
+@class Transition begin
+    __freq::Real
+    __dipole::Real
+
+    function __class_init__(self, kws::Dict{Symbol, Any})
+        if haskey(kws, :freq)
+            self.__freq = pop!(kws, :freq)
+        elseif haskey(kws, :lamb)
+            self.__freq = c_0 / pop!(kws, :lamb)
+        else
+            error("no arguments to initialize frequency")
+        end
+        if haskey(kws, :dipole)
+            self.__dipole = pop!(kws, :dipole)
+        else
+            error("no arguments to initialize dipole moment")
+        end
+        if !isempty(kws)
+            error("too many arguments to initialize Transition")
+        end
+    end
+
+    function __class_init__(self; _kws...)
+        self.__class_init__(Dict{Symbol, Any}(_kws))
+    end
+
+    function omega(self)
+        return self.__freq * 2 * π
+    end
+
+    function freq(self)
+        return self.__freq
+    end
+
+    function lamb(self)
+        return c_0 / self.__freq
+    end
+
+    function dipole(self)
+        return self.__dipole
+    end
+
+    function ac_stark(self, freq::Real, I::Real=1.):
+        return -(self.__dipole^2 * I / ħ / c_0 / ϵ_0 / 4 / π
+                 * (1 / (self.__freq - freq) + 1 / (self.__freq + freq)))
+    end
 end
